90c00c773b153a9718cc7de029b47617c28e3693
Evelio Tarazona Cáceres <evelio@evelio.info>
2013-08-07 16:56:05 -0700

    Initial import commit

---
+++ Fragments/src/main/java/info/evelio/fragments/LoggerFragment.java
@@ -0,0 +1,128 @@
+package info.evelio.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public final class LoggerFragment extends Fragment {
+
+  private TextView mLogView;
+  private StringBuilder mLogBuilder;
+
+  @Override
+  public void onAttach(Activity activity) {
+    super.onAttach(activity);
+
+    log("onAttach");
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    log("onCreate");
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    log("onCreateView");
+    return inflater.inflate(R.layout.fragment_logger, container, false);
+  }
+
+  @Override
+  public void onViewCreated(View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    mLogView = (TextView) view.findViewById(R.id.log_view);
+    final TextView nameView = (TextView) view.findViewById(R.id.name_view);
+    nameView.setText(getTag());
+
+    log("onViewCreated");
+  }
+
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    log("onActivityCreated");
+  }
+
+  @Override
+  public void onViewStateRestored(Bundle savedInstanceState) {
+    super.onViewStateRestored(savedInstanceState);
+
+    log("onViewStateRestored");
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+
+    log("onStart");
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+
+    log("onResume");
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+
+    log("onPause");
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+
+    log("onStop");
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    log("onDestroyView");
+    mLogView = null;
+  }
+
+  @Override
+  public void onDetach() {
+    super.onDetach();
+
+    log("onDetach");
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+
+    log("onDestroy");
+  }
+
+
+  private void log(String msg) {
+    msg = getTag() + "[" + hashCode() + "] " + msg;
+    StringBuilder builder = mLogBuilder;
+    if (builder == null) {
+      builder = new StringBuilder(msg);
+      mLogBuilder = builder;
+    } else {
+      builder.append(msg);
+    }
+
+    builder.append("\n");
+
+    Log.d("fr:log", msg);
+    final TextView view = mLogView;
+    if (view != null) {
+      view.setText(builder.toString());
+    }
+  }
+}
---
+++ Fragments/src/main/java/info/evelio/fragments/MainActivity.java
@@ -0,0 +1,32 @@
+package info.evelio.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+
+  private static final String A = "A";
+  private static final String B = "B";
+  private static final String RETAIN = "RE";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+
+    if (savedInstanceState == null) {
+      addFragment(A);
+      addFragment(B);
+      addFragment(RETAIN).setRetainInstance(true);
+    }
+  }
+
+  private Fragment addFragment(String tag) {
+    final Fragment fr = new LoggerFragment();
+    getSupportFragmentManager().beginTransaction().add(R.id.fragment_root, fr, tag).commit();
+    return fr;
+  }
+
+}
